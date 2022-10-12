687397035a750d299408788bf77e9329e3def378
0511uni <105358271+0511uni@users.noreply.github.com>
2022-10-13 04:24:47 +0900

    Import SampleProject

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[SerializeField]
+    public float speed = 15;
+
+    public float jumppower = 8;
+    new
+    #region//プライベート変数　SpriteRenderer
+        SpriteRenderer renderer;
+    Rigidbody2D rbody;
+    #endregion
+
+    #region//プライベート変数  bool
+    bool pushFlag;
+    private bool jumpFlag;
+    #endregion
+
+    public void LButtonDown()
     {
-        
+        transform.Translate(-20, 0, 0);
+
+        renderer.flipX = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RButtonDown()
     {
-        
+        transform.Translate(20, 0, 0);
+
+        renderer.flipX = false;
+    }
+
+    public void UButtonDown()
+    {
+        transform.Translate(0, 20, 0);
+
+        if (pushFlag == false)
+        {
+            jumpFlag = true;
+            pushFlag = true;
+        }
+        else
+        {
+            pushFlag = false;
+        }
+    }
+
+    public void DButtonDown()
+    {
+        transform.Translate(0, -20, 0);
+    }
+
+    void Start()
+    {
+        renderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        renderer.flipX = false;
+
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    //void Update()
+    //{
+
+    //}
+
+    void FixedUpdate()
+    {
+        if (jumpFlag)
+        {
+            jumpFlag = false;
+            rbody.AddForce(new Vector2(0, jumppower), ForceMode2D.Impulse);
+        }
     }
 }
