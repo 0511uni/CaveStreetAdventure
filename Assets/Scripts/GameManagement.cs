cd79084b962dc6297cc14a1da7310b1b968125a4
0511uni <105358271+0511uni@users.noreply.github.com>
2022-10-14 18:16:23 +0900

    Goalのリファクタリング
    主にコンポーネント周り

---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// ゲーム全体を制御する
+/// </summary>
+public class GameManagement : MonoBehaviour
+{
+    #region//インスペクターで設定する
+
+    public Text winText;  //  リザルトのUI
+
+    #endregion
+
+    #region//プライベート変数  bool
+
+    bool gameClear = false; //ゲームクリアーしたら操作を無効にする
+    
+    #endregion
+
+
+    void Start()
+    {
+        winText.text = " ";
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GameClear()
+    {
+        gameClear = true;
+        winText.text = "You Win!";
+    }
+}
---
+++ Assets/Scripts/PlayerLifeManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// プレイヤーのライフ管理
+/// </summary>
+public class PlayerLifeManagement : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            {
+                GameObject gameManager = GameObject.Find("GameManager");
+                gameManager.GetComponent<GameManagement>().GameClear();
+            }
+        }
+    }
+}
