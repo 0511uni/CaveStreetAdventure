d42267c66e69fe672230505741bd4da72f93e775
0511uni <uni202205_11@telework.to>
2022-10-17 07:38:11 +0900

    リトライボタンを効かせるScriptとアタッチ
    リトライボタンが使えるようになった

---
+++ Assets/OnRetryButtonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnRetryButtonController : MonoBehaviour
+{
+    public void OnRetry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/PlayerLifeManagement.cs
+++ Assets/Scripts/PlayerLifeManagement.cs
@@ -14,5 +14,6 @@ public class PlayerLifeManagement : MonoBehaviour
     //void Update()
     //{
 
-    //}   
+    //}
+
 }
