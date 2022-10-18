a1bf32e0d756eeaa2d594e3d5e80be386d607efd
0511uni <105358271+0511uni@users.noreply.github.com>
2022-10-18 16:49:48 +0900

    Create LiftDownMove.cs

---
+++ Assets/LiftDownMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftDownMove : MonoBehaviour
+{
+    #region//インスペクターで設定する
+    [Header("移動速度")] public float speed;
+
+
+    [Header("重力")] public float gravity;
+    #endregion
+
+    #region//プライベート変数
+    private Rigidbody2D rbody = null;
+    #endregion
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(speed, -gravity);
+    }
+}
