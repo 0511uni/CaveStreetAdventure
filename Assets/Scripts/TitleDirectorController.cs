ee9db54eb0abd485aac85ceb97d961216aa73d60
0511uni <uni202205_11@telework.to>
2022-10-20 15:04:28 +0900

    タイトルSceneからゲームメインにScene遷移完了

--- Assets/Scripts/TitleDirectorController.cs
+++ Assets/Scripts/TitleDirectorController.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleDirectorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //　スタートボタンを押したら実行する
+    public void GameStart()
     {
-        
+        print("a");
+        SceneManager.LoadScene("GameMain");
     }
 
-    // Update is called once per frame
-    void Update()
+    //　RANKING Buttonを押したら実行する
+    public void RankingStart()
     {
-        
+        print("b");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Ranking_Jump Adventure Land");
     }
+
+    //　ゲーム終了ボタンを押したら実行する
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+        Application.Quit();
+#endif
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.SetResolution(400, 710, false, 60);
+    }
+
 }
