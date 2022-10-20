ca266ccc1aca437d1832a05ae364e46be79efdb6
0511uni <105358271+0511uni@users.noreply.github.com>
2022-10-20 15:54:31 +0900

    タイトル⇔ゲームScene行き来可能に。

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 /// <summary>
 /// ゲーム全体を制御する
 /// </summary>
 public class GameManagement : MonoBehaviour
 {
-    void Start()
+    //　タイトルボタンを押したら実行する
+    public void TitleBackBottan()
     {
-        
+        print("a");
+        SceneManager.LoadScene("TitleMenuScene");
     }
-
-    void Update()
-    {
-        
-    }  
 }
