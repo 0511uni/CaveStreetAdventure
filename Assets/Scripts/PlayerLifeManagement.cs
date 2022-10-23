6f9fe1d5f002120e464dd556b5c041c0a8603011
0511uni <uni202205_11@telework.to>
2022-10-23 20:12:36 +0900

    再起動したらエラーが直った

--- Assets/Scripts/PlayerLifeManagement.cs
+++ Assets/Scripts/PlayerLifeManagement.cs
@@ -9,49 +9,15 @@ public class PlayerLifeManagement : MonoBehaviour
 {
     #region//インスペクターで設定する
 
-    //[SerializeField]
-    //GameObject enemy;
-
     [SerializeField]
     Text scoreText;  //  スコアのUI
 
-    //[SerializeField]
-    //Text scoreTextWin;  //  スコアのUI
-
-    //[SerializeField]
-    //Text scoreTextGameOver;
-
     public int score; //  スコア
-    //[SerializeField]
-    //int score2; //  スコア
+ 
     public int highScore; //ハイスコア用変数
 
     [SerializeField]
     Text highScoreText; //ハイスコアを表示するText
-
-    //[SerializeField]
-    //Text highScoreTextWin; //Winハイスコアを表示するText
-
-    //[SerializeField]
-    //Text highScoreTextGameOver;
-
-    //[SerializeField]
-    //Text gameOverText; // ゲームオーバーUI
-
-    //[SerializeField]
-    //GameObject resultRSButton;
-
-    //[SerializeField]
-    //GameObject resultGameOverPanel;
-
-    //[SerializeField]
-    //GameObject resultGameOverIcon;
-
-    //[SerializeField]
-    //GameObject scoreUI;
-
-    //[SerializeField]
-    //GameObject buttonController;
 
     [SerializeField]
     GameManagement gameManagement;
@@ -59,8 +25,6 @@ public class PlayerLifeManagement : MonoBehaviour
 
     #region//プライベート変数 ハイスコアー保存キー
     
-    
-
     string key = "HIGH SCORE"; //ハイスコアの保存先キー
 
     //bool gameOver;
@@ -70,15 +34,6 @@ public class PlayerLifeManagement : MonoBehaviour
     {
         score = 10;
         scoreText.text = "Count: " + score.ToString();
-        //scoreTextWin.enabled = false;
-        //scoreTextGameOver.enabled = false;
-        //highScoreTextWin.enabled = false;
-        //highScoreTextGameOver.enabled = false;
-        //gameOverText.enabled = false;
-        //resultRSButton.SetActive(false);
-        //resultGameOverPanel.SetActive(false);
-        //resultGameOverIcon.SetActive(false);
-
         highScore = PlayerPrefs.GetInt(key, 0);
         //保存しておいたハイスコアをキーで呼び出し取得し保存されていなければ0になる
         highScoreText.text = "High Score: " + highScore.ToString();
@@ -138,23 +93,6 @@ public class PlayerLifeManagement : MonoBehaviour
             //  UI の表示を最新します
             SetCountText();
         }
-
-
-        ////  スコアの表示を最新
-        //scoreText.text = "Score: " + score.ToString();
-        //// ハイスコアより現在スコアが高い時
-        //if (score > highScore)
-        //{
-
-        //    highScore = score;
-        //    //ハイスコア更新
-
-        //    PlayerPrefs.SetInt(key, highScore);
-        //    //ハイスコアを保存
-
-        //    highScoreText.text = "HighScore: " + highScore.ToString();
-        //    //ハイスコアを表示
-        //}
     }
 
     // UI の表示を最新する
@@ -176,49 +114,15 @@ public class PlayerLifeManagement : MonoBehaviour
             highScoreText.text = "HighScore: " + highScore.ToString();
             //ハイスコアを表示
         }
-        //  スコアの表示を最新
-        //scoreText.text = "Score: " + score.ToString();//Count: 
-
-        //scoreTextGameOver.text = "Score: " + score.ToString();//Count:
-
-        //highScoreTextGameOver.text = "High Score: " + highScore.ToString();
 
         if (score <= 0)
-        {//  リザルドの表示を最新
-
+        {
             gameManagement.GameOver();
-
-            //gameOver = true;
-            //print("gameovr");
-
-            //enemy.GetComponent<EnemyRoundTripAct>().enabled = false; // 敵を止める
-
-            //GetComponent<PlayerController>().enabled = false; // 自分のとこのコンポーネントを止める
-
-            //gameOverText.enabled = true;
-
-            //resultRSButton.SetActive(true);
-
-            //resultGameOverPanel.SetActive(true);
-
-            //resultGameOverIcon.SetActive(true);
-
-            //scoreTextGameOver.enabled = true;
-
-            //highScoreTextGameOver.enabled = true;
-
-            //scoreUI.SetActive(false);
-
-            //buttonController.SetActive(false);
         }
     }
 
     public void GameClearScore()
     {
-        //scoreTextWin.text = "Score: " + score.ToString();
-
-
-        //highScoreTextWin.text = "High Score: " + highScore.ToString();
 
         // ハイスコアより現在スコアが高い時
         if (score > highScore)
