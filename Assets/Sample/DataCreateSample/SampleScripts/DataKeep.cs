aa4f76dc5a3568ab016cc1124070542b053b0676
0511uni <uni202205_11@telework.to>
2022-10-24 00:09:23 +0900

    再クローンしてScriptバグ直ったので再開
    SampleScene作成

---
+++ Assets/Sample/DataCreateSample/SampleScripts/DataKeep.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data")]
+public class DataKeep : ScriptableObject
+{
+    public int score;
+}
