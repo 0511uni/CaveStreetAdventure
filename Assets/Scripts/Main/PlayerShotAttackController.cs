26f3e614187be517588d258704db51d5b437a432
0511uni <105358271+0511uni@users.noreply.github.com>
2022-10-27 21:02:18 +0900

    オブジェクトがインスタンティエイトされた

---
+++ Assets/Scripts/Main/PlayerShotAttackController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotAttackController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+    //public Transform shotTarget;
+    //public Transform shotTargetSpawn;
+
+    public void SButtonDown()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        //GetComponent<AudioSource>().Play();
+    }
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform shotSpawn in shotSpawn)
+        {
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+}
