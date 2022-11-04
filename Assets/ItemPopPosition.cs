bd464c27699407afa56a79c73d04396546556947
0511uni <105358271+0511uni@users.noreply.github.com>
2022-11-04 17:38:56 +0900

    ギミック効かないバグ解消

---
+++ Assets/ItemPopPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// アイテムが出現する
+/// </summary>
+public class ItemPopPosition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject item;
+
+    /// <summary>
+    /// トリガーとの接触時に呼ばれるコールバック
+    /// </summary>
+    /// <param name="hit">接触した相手</param>
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        // Wellに当たったら
+        if (hit.CompareTag("Wall"))
+        {
+            item.transform.Translate(0, 2, 0);
+        }     
+    }
+}
--- Assets/Scripts/Main/ItemPoint.cs
+++ Assets/Scripts/Main/ItemPoint.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 /// </summary>
 public class ItemPoint : MonoBehaviour
 {
-    // トリガーとの接触時に呼ばれるコールバック
-    void OnTriggerEnter2D(Collider2D hit)
-    {
-        // 接触対象はPlayerタグですか？
-        if (hit.CompareTag("Player"))
-        {
+    //// トリガーとの接触時に呼ばれるコールバック
+    //void OnTriggerEnter2D(Collider2D hit)
+    //{
+    //    // 接触対象はPlayerタグですか？
+    //    if (hit.CompareTag("Player"))
+    //    {
+            
+    //    }
 
-            // 何らかの処理
+        
+    //}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("atatta???");
+            transform.Translate(0, 1, 0);
+            other.transform.Translate(0, -1, 0);
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
             //当たったオブジェクトを消す
             Destroy(this.gameObject);
         }
