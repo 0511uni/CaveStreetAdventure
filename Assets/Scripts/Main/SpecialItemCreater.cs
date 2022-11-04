a904dee5f2dffa6b5cce85e7ec4d0751dd4862b2
0511uni <uni202205_11@telework.to>
2022-11-05 03:23:15 +0900

    ラストスペシャルアイテム実装完了

---
+++ Assets/Scripts/Main/SpecialItemCreater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムがアイテムを呼ぶ
+/// </summary>
+public class SpecialItemCreater : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject lastBigItems;
+
+    void Start()
+    {
+        lastBigItems.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            lastBigItems.SetActive(true);
+        }   
+    }
+}
