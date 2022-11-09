3007c5b9b8e244d8401e55506bb770c59b666724
0511uni <uni202205_11@telework.to>
2022-11-10 07:08:54 +0900

    エンドテロップ繰り返し完成
    シンプルなやり方でできた。

--- Assets/WarpPointUI.cs
+++ Assets/WarpPointUI.cs
@@ -9,71 +9,96 @@ using UnityEngine.UI;
 /// </summary>
 public class WarpPointUI : MonoBehaviour
 {
-    GameObject obj;
-
-    RectTransform rectTransform;
-
-    [Header("-----------------------------\n" +
-        "【 一方通行を作る方法 】\n" +
-        "移動先オブジェクトのコライダーの\n" +
-        "isTriggerのチェックを外すだけ！\n" +
-        "-----------------------------")]
-    [Header("ワープさせる相手オブジェクト\n" +
-        "※ 双方をアタッチ必須 ※")]
-    public WarpPointUI transObj;
-
-    Vector2 transVec;
-
-    //移動状態を表すフラグ
-    [Header("移動状態を表すフラグ")]
     [SerializeField]
-    bool moveStatus;//移動のみ
+    RectTransform end;
+
+    //RectTransform obj;
+
+    //RectTransform transVec;
+
+    //[Header("-----------------------------\n" +
+    //    "【 一方通行を作る方法 】\n" +
+    //    "移動先オブジェクトのコライダーの\n" +
+    //    "isTriggerのチェックを外すだけ！\n" +
+    //    "-----------------------------")]
+    //[Header("ワープさせる相手オブジェクト\n" +
+    //    "※ 双方をアタッチ必須 ※")]
+    //public WarpPointUI transObj;
+
+    ////Vector2 transVec;
+
+    ////移動状態を表すフラグ
+    //[Header("移動状態を表すフラグ")]
+    //[SerializeField]
+    //bool moveStatus;//移動のみ
 
     void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
+        end = GetComponent<RectTransform>();
 
-        rectTransform.anchoredPosition = transVec;
-        
-        transVec = transObj.rectTransform.anchoredPosition;
 
-        //RectTransform rectTransform = transform as RectTransform;
+        //transVec = GetComponent<RectTransform>();
 
-        //transVec = rectTransform.anchoredPosition;s
+        ////transObj = GetComponent<RectTransform>();
 
-        //transVec = transObj.anchoredPosition;
+        ////transVec.anchoredPosition = transObj;//rectTransform.anchoredPosition;
 
-        //初期では移動可能なためTrue
-        moveStatus = true;
+        ////transVec = transform as RectTransform;
+
+        //transVec = transObj.transVec;
+
+        ////transVec = transObj.transform.position;
+
+        ////初期では移動可能なためTrue
+        //moveStatus = true;
+    }
+
+    private void Update()
+    {
+        end.anchoredPosition += new Vector2(0, 5);
     }
 
     /// <summary>
     /// オブジェクトが引き金でイベントが発生する
     /// </summary>
     /// <param name="other">オブジェクト</param>
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D hit)
     {
-        obj = GameObject.Find(other.name);
-        //自分が移動可能なとき移動する。
-
-
-        if (moveStatus)
+        if (hit.CompareTag("end"))
         {
+            Debug.Log("a");
+            end.anchoredPosition += new Vector2(0, -1700);
+        }
 
-            //移動先は直後移動できないようにする 
-            transObj.moveStatus = false;
-            obj.transform.position = transVec;
-        } // 移動   
+
+        //obj = other.GetComponent<RectTransform>();
+        ////obj = GameObject.Find(other.name);
+
+        ////自分が移動可能なとき移動する。
+
+        ////if (other.CompareTag("end"))
+        ////{
+
+        ////}
+
+
+        //if (moveStatus)
+        //{
+
+        //    //移動先は直後移動できないようにする 
+        //    transObj.moveStatus = false;
+        //    obj = transVec;
+        //} // 移動   
     }
 
-    /// <summary>
-    /// 物体と離れた直後呼ばれる 
-    /// </summary>
-    /// <param name="other">オブジェクト</param>
-    void OnTriggerExit2D(Collider2D other)
-    {
-        //移動可能にする。
-        moveStatus = true;
+    ///// <summary>
+    ///// 物体と離れた直後呼ばれる 
+    ///// </summary>
+    ///// <param name="other">オブジェクト</param>
+    //void OnTriggerExit2D(Collider2D other)
+    //{
+    //    //移動可能にする。
+    //    moveStatus = true;
 
-    }
+    //}
 }
