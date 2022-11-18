710a7e030f5650c3f8060b105bd56c918ad60a46
0511uni <uni202205_11@telework.to>
2022-11-18 21:26:05 +0900

    Update GameStatus.cs
    １

--- Assets/Scripts/Main/GameStatus.cs
+++ Assets/Scripts/Main/GameStatus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 [CreateAssetMenu(menuName = "GameStatus")]
 public class GameStatus : ScriptableObject
